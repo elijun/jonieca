d95ba95cee6b869021d6c2a55948ded89df6eaaa
elijun <joniecaelijun@gmail.com>
2025-06-21 01:01:20 +0800

    test

--- test.java
+++ test.java
@@ -1,1 +1,17 @@
 
+public class test {
+    public static void main(String[] args) {
+        int x = 1;
+        testing t1 = new testing();
+        t1.name = "Diane";
+        while (x < 10){
+            System.out.println(x + t1.name);
+            x++;
+        }
+    }
+}
+
+class testing{
+    String name;
+    int age;
+}
