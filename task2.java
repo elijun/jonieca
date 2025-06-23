c5dc575cb57329c7fc13fecc6b7d0eb7fcf96d35
elijun <joniecaelijun@gmail.com>
2025-06-23 11:51:11 +0800

    task2 Chapter 2 part 1

---
+++ task2.java
@@ -0,0 +1,15 @@
+public class task2 {
+    public static void main(String[] args) {
+        String H = "H";
+        int three = 3;
+        int one = 1;
+        byte zero = 0;
+        String w = "w";
+        String d = "d";
+        String r = "r";
+        float twopointow = 2.0f;
+        boolean T = true;
+        String output = H + three + one + one + zero + " " + w + zero + one + r + d + " " + twopointow + " " + T;
+        System.out.println(output);
+    }
+}
