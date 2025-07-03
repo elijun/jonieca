a76915ad3f126f40a252f9ef023a4a9e0c7ea33d
elijun <joniecaelijun@gmail.com>
2025-07-03 10:54:15 +0800

    task5 = ArrayList

--- task5.java
+++ task5.java
@@ -10,9 +10,13 @@ public class task5 {
         System.out.print("Third number: ");
         int num3 =  scan.nextInt();
 
-        List list = Arrays.asList(num1, num2, num3);
-        System.out.println(list);
-        Collections.sort(list);
-        System.out.println("Largest Number: "+list.getLast());
+        if (num1 == num2 && num2 == num3 && num1 == num3){
+            System.out.println("All numbers are equal");
+        } else {
+            List list = Arrays.asList(num1, num2, num3);
+            System.out.println(list);
+            Collections.sort(list);
+            System.out.println("Largest Number: "+list.getLast());
+        }
     }
 }
